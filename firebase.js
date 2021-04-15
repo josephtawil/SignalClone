6d57bd84ad371fbc4e5aecbf4b4ac4eb45dff7b3
Joseph <jtawil2015@gmail.com>
2021-04-14 18:29:18 -0700

    added firebase credentials

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRFtYO6AUjcp56oGMC1gkmeVCh5Rb3EyQ",
+    authDomain: "signalclone-13cde.firebaseapp.com",
+    projectId: "signalclone-13cde",
+    storageBucket: "signalclone-13cde.appspot.com",
+    messagingSenderId: "534976223374",
+    appId: "1:534976223374:web:04a7df491792f3a14451df"
+  };
