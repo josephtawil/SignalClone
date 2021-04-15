b22148ddbd38ad1e6dfa0d23a647d6bad2775cf2
Joseph <jtawil2015@gmail.com>
2021-04-14 18:50:42 -0700

    added Stack page

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "./screens/LoginScreen";
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Signal Clone</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Screen name='Login' component={LoginScreen}/>
+    </NavigationContainer>
+
+  
   );
 }
 
